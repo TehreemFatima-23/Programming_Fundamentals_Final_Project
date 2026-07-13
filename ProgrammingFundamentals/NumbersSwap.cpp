d2257bf8bf72ddcd831b31112aabea199c5c9d6e
TehreemFatima-23 <tehreem.twice2005@gmail.com>
2026-07-14 00:12:07 +0500

    Addition of function (Swapping without using third variable)

--- ProgrammingFundamentals/NumbersSwap.cpp
+++ ProgrammingFundamentals/NumbersSwap.cpp
@@ -1,17 +1,43 @@
 #include <iostream>
 using namespace std;
-int main(){
+
+void swapWithoutThird(){
+    int a, b;
+
+    cout<<"-----Swapping using a third variable-----"<<endl;
+    cout<<"Enter first Number: ";
+    cin>>a;
+    cout<<"Enter second Number: ";
+    cin>>b;
+
+    a = a + b;
+    b = a - b;
+    a = a - b;
+
+    cout<<"Swap is: "<<endl<<"First Number: "<<a<<endl<<"Second Number: "<<b<<endl;
+
+}
+void swapUsingThird(){
+
     int num1, num2;
-
-    cout<<"Enter the Numbers you want to swap with each other: ";
-    cin>>num1>>num2;
-
     int temp;
+    
+    cout<<"-----Swapping using a third variable-----"<<endl;
+    cout<<"Enter Number-1: ";
+    cin>>num1;
+    cout<<"Enter Number-2: ";
+    cin>>num2;
+
     temp = num1;
     num1 = num2;
     num2 = temp;
 
-    cout<<"The numbers after swapping are: "<<num1<<" "<<num2;
+    cout<<"Swap is: "<<endl<<"Number-1: "<<num1<<endl<<"Number-2: "<<num2<<endl;
+}
+int main(){
+
+    swapWithoutThird();
+    swapUsingThird();
 
     return 0;
 }
