73cc4b104a4a8e96d9e77837838c21b3b36becc3
TehreemFatima-23 <tehreem.twice2005@gmail.com>
2026-07-23 23:52:20 +0500

    Addition of comments for better understandingof logic

--- ProgrammingFundamentals/LeapYear.cpp
+++ ProgrammingFundamentals/LeapYear.cpp
@@ -6,6 +6,7 @@ int year;
 std::cout<<"Enter a year: ";
 std::cin>>year;
 
+//To check if the entered year is leap or not
 if (year%400==0 || year%4==0 && year%100 != 0){
     std::cout<<"Leap year";
 }
